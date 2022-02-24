cc93afe34cf0390b3a6c2f5af98c01da342db7ce
jh <97775986+jhp5234@users.noreply.github.com>
2022-02-24 17:05:28 +0900

    express + typescript 적용

---
+++ src/app.ts
@@ -0,0 +1,15 @@
+import express, { Request, Response, NextFunction } from "express";
+
+const app = express();
+
+app.get("/", (req: Request, res: Response, next: NextFunction) => {
+  res.send("Hello, World!");
+});
+
+app.listen("1234", () => {
+  console.log(`
+  ################################################
+  🛡️  Server listening on port: 1234🛡️
+  ################################################
+`);
+});
