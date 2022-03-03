d3d8a54ee312cbf92ed791777ea57f9082ba8174
jh <97775986+jhp5234@users.noreply.github.com>
2022-03-03 23:03:55 +0900

    코드 줄 맞추기

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,13 @@
-import express, { Application, Request, Response, NextFunction } from "express";
+import express, { Application, Request, Response, NextFunction } from 'express';
 
 const app: Application = express();
 
-app.get("/", (req: Request, res: Response, next: NextFunction) => {
-  res.send("Hello, World!");
+app.get('/', (req: Request, res: Response, next: NextFunction) => {
+    res.send('Hello, World!');
 });
 
-app.listen("1234", () => {
-  console.log(`
+app.listen('1234', () => {
+    console.log(`
   ################################################
   🛡️ Server listening on port: 1234 🛡️
   ################################################
