6df1b2fd5f70e4aaaad6b15bde1eab5856e31eff
jh <97775986+jhp5234@users.noreply.github.com>
2022-03-03 21:52:28 +0900

    Prettier 설정

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,13 @@
-import express, { Request, Response, NextFunction } from "express";
+import express, { Request, Response, NextFunction } from 'express';
 
 const app = express();
 
-app.get("/", (req: Request, res: Response, next: NextFunction) => {
-  res.send("Hello, World!");
+app.get('/', (req: Request, res: Response, next: NextFunction) => {
+    res.send('Hello, World!');
 });
 
-app.listen("1234", () => {
-  console.log(`
+app.listen('1234', () => {
+    console.log(`
   ################################################
   🛡️ Server listening on port: 1234 🛡️
   ################################################
