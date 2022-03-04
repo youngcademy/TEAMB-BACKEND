c37c8885fef654676e7380af5095741d54368b85
goops2000 <goops2000@naver.com>
2022-03-04 18:21:32 +0900

    type/jsonwebtoken 설치,test페이지 토큰 생성

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,22 @@
-import express, { Application, Request, Response, NextFunction } from "express";
+import express, { Application, Request, Response, NextFunction } from 'express';
+import jwt from 'jsonwebtoken';
 
 const app: Application = express();
 
-app.get("/", (req: Request, res: Response, next: NextFunction) => {
-  res.send("Hello, World!");
+app.get('/', (req: Request, res: Response, next: NextFunction) => {
+    res.send('Hello, World!');
 });
 
-app.listen("1234", () => {
-  console.log(`
+app.get('/test', function (req, res) {
+    const user = { id: 3 };
+    const token = jwt.sign({ user }, 'my_secret_key');
+    res.send({
+        token: token,
+    });
+});
+
+app.listen('1234', () => {
+    console.log(`
   ################################################
   🛡️ Server listening on port: 1234 🛡️
   ################################################
