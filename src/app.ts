8d776875ace08f5a1f02dc1b9a5da4cff35c64fb
jh <97775986+jhp5234@users.noreply.github.com>
2022-03-11 20:02:10 +0900

    jwt 생성 & 토큰유효성 검사 미들웨어

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,10 @@
+require('dotenv').config(); // .env 파일에 있는 환경 변수를 process.env로 로드해줌
+
 import 'reflect-metadata';
-import express, { Request, Response } from 'express';
+import express, { Request, Response, NextFunction } from 'express';
 import bodyParser from 'body-parser';
 import cors from 'cors';
+const jwt = require('jsonwebtoken'); // JWT 생성 및 검증
 
 import sequelize from './sequelize';
 import { Dog } from './models';
@@ -11,6 +14,50 @@ const app = express();
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+
+// Access token 생성
+const generateAccessToken = (id: string) => {
+    return jwt.sign({ id }, process.env.ACCESS_TOKEN_SECRET, {
+        expiresIn: '10m',
+    });
+};
+
+// Refresh token 생성
+const generateRefreshToken = (id: string) => {
+    return jwt.sign({ id }, process.env.REFRESH_TOKEN_SECRET, {
+        expiresIn: '30 days',
+    });
+};
+
+// Access token의 유효성 검사
+const authenticateAccessToken = (
+    req: any,
+    res: Response,
+    next: NextFunction
+) => {
+    // 요청의 헤더에 포함된 토큰을 받아옴
+    const authHeader = req.headers['authorization'];
+    const token = authHeader && authHeader.split(' ')[1];
+
+    if (!token) {
+        console.log('Wrong token format or token is not sended.');
+        return res.sendStatus(400);
+    }
+
+    // 받아온 토큰 검사
+    jwt.verify(
+        token,
+        process.env.ACCESS_TOKEN_SECRET,
+        (error: boolean, user: any) => {
+            if (error) {
+                console.log(error);
+                return res.sendStatus(403);
+            }
+            req.user = user;
+            next();
+        }
+    );
+};
 
 app.get('/dogs', async (req: Request, res: Response): Promise<Response> => {
     const allDogs: Dog[] = await Dog.findAll();
